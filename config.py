31578046ecc4e4c2c1f3477cf6de2fa60485de1a
marvdaluz <134108840+marvdaluz@users.noreply.github.com>
2026-07-27 18:52:37 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,24 +1,29 @@
 # --- CREDENCIAIS WI-FI ---
 WIFI_SSID = "Ubuntu"
 WIFI_PASS = "27734056"
+
 # --- CONFIGURAÇÃO GITHUB / SENKO OTA ---
 GITHUB_USER = "marvdaluz"
 GITHUB_REPO = "esp8266-firmware-base"
 GITHUB_BRANCH = "main"
-ARQUIVOS_OTA = ["boot.py", "app.py", "senko.py", "main.py"]
+ARQUIVOS_OTA = ["boot.py", "app.py", "senko.py", "main.py","config.py"]
+
 # --- CREDENCIAIS MQTT ---
 MQTT_BROKER = "10.103.1.30"
 MQTT_PORT = 1883
 MQTT_USER = "mqtt_user"
 MQTT_PASS = "p300b795"
 CLIENT_ID = "esp8266_lampada_01"
+
 # --- HARDWARE ---
 PIN_RELE = 5
+
 # --- TÓPICOS MQTT ---
 TOPICO_CONFIG = "homeassistant/switch/esp8266_lampada_01/config"
 TOPICO_ESTADO = "casa/lampada_01/state"
 TOPICO_COMANDO = "casa/lampada_01/set"
 TOPICO_OTA = "casa/lampada/ota"
+
 # --- PAYLOAD MINIMALISTA ---
 CONFIG_PAYLOAD = {
     "name": "Lampada Sala",
@@ -30,7 +35,5 @@ CONFIG_PAYLOAD = {
     "device": {
         "identifiers": ["esp8266_lampada_01"],
         "name": "Rele Sala"
-    }
-}
     }
 }
